4887c9201e4d750261597fd476608b38ec52d59e
techleadpro <techitleadpro@gmail.com>
2025-04-11 09:51:54 -0400

    added 1 files

--- quotes_api/config/routes.rb
+++ quotes_api/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :quotes, only: [:index] do
+      resources :quotes, only: [:index] do
         collection do
           get 'random'
         end
